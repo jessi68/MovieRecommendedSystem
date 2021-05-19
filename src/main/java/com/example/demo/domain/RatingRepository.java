8eaa9009055d03ee3e4562ecb810b6bce20f5d9d
jessi68 <lynn312.unist.ac.kr>
2021-05-19 23:00:54 +0900

    feat: steam api 와 jpaQuery 를 활용해 특정 movie_id 를 가진 영화들의 평점 합 구현

--- src/main/java/com/example/demo/domain/RatingRepository.java
+++ src/main/java/com/example/demo/domain/RatingRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class RatingRepository {
@@ -25,5 +26,14 @@ public class RatingRepository {
         return entityManager.createQuery("select r from rating as r").getResultList();
     }
 
+    public int totalRatingsFindBy(int movie_id) {
+        int totalScore = 0;
+        List<Rating> ratings = entityManager.createQuery("select r from rating as r join r.movie as m where m.id = #{#movie_id}").getResultList();
+
+        totalScore = ratings.stream()
+                            .map(Rating::getScore)
+                            .collect(Collectors.summingInt(Integer::intValue));
+        return totalScore;
+    }
     //public List<>
 }
