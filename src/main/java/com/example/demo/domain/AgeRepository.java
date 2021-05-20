6f5fd190a05648d705b4ef3fd2a2d7166b16b7fe
jessi68 <lynn312.unist.ac.kr>
2021-05-20 11:01:11 +0900

    feat: 대표값을 가진 age 객체 반환하는 함수 구현

--- src/main/java/com/example/demo/domain/AgeRepository.java
+++ src/main/java/com/example/demo/domain/AgeRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AgeRepository extends JpaRepository<Age, Integer> {
 
+    Age findByRepresentative(Integer representative);
 }
