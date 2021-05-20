095e3573c772523e93f81f55018179a535a1c851
jessi68 <lynn312.unist.ac.kr>
2021-05-20 10:16:23 +0900

    feat: movie_id 를 가진 영화에 대한 평점의 총합을 계산해주는 getapi 구현

--- src/main/java/com/example/demo/controller/RatingController.java
+++ src/main/java/com/example/demo/controller/RatingController.java
@@ -1,7 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Rating;
+import com.example.demo.domain.RatingRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MovieController {
+public class RatingController {
+    private final RatingRepository ratingRepository;
+
+
+    public RatingController(RatingRepository ratingRepository) {
+        this.ratingRepository = ratingRepository;
+    }
+
+    @GetMapping("/ratings/{movie_id}")
+    int totalRatingBy(@PathVariable int movie_id) {
+        return ratingRepository.totalRatingsFindBy(movie_id);
+    }
+
 }
+
